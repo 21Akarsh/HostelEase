866eb9edf67012cabfbdbcda085f417553b35437
akarsh <21akarsh@gmail.com>
2025-04-03 08:44:37 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,16 @@ import complaintRoutes from "./routes/complaintRoutes.js";
 import studentRoutes from "./routes/studentRoutes.js";
 import wardenRoutes from "./routes/wardenRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import otpRoutes from "./routes/otpRoutes.js"; // Import OTP routes
+import otpRoutes from "./routes/otpRoutes.js"; 
 
 const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Allow only your frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  credentials: true // Allow cookies and authorization headers
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true 
 }));
 
 
@@ -23,7 +23,7 @@ app.use("/", complaintRoutes);
 app.use("/", studentRoutes);
 app.use("/", wardenRoutes);
 app.use("/", userRoutes);
-app.use("/", otpRoutes); // Use OTP routes
+app.use("/", otpRoutes); 
 
 app.listen(3000, () => {
   console.log("Application is running on port 3000");
